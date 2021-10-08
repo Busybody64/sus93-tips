0faf55a7259ade45556994983c765dd74ab241cf
Busybody64 <92069026+Busybody64@users.noreply.github.com>
2021-10-08 18:59:57 -0500

    Update example-module.js

--- example-module.js
+++ example-module.js
@@ -1,4 +1,1 @@
-(async function exampleModule() {
-    $log(`Hello, world!
-This is an example module implementation for sus93.`);
-})
+$alert({msg:"If you're seeing this, then it worked!",center:!1,btnOk:"OK",title:"Error",animationOut:"none",icon:"https://i.imgur.com/zzz1KA1.png"})
